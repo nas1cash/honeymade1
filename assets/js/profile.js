42c626a206bfdc79c45a911e10ac2c8e3020108b
nas1cash <nas1cash@gmail.com>
2021-05-28 06:43:50 +0100

    public

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -3,6 +3,12 @@ const nav itemContainer=document.getElementbyId("nav-item-wrapper");
 
 
 
-function toggle Navbar(){
+function toggle Navbar{(<div>
+     <div class="nav-items" id="nav-item-wrapper">
+         <a href= "#">HOME</a>
+         <a href="./public1/menu.html" class="nav-links"> MENU </a>
+         <a href="./public1/product.html" class="nav-links">STORE</a>
+         <a href="#">ABOUT</a>
+         <a href="">CONTACT</a>){
       navItemContainer.ClassList.toggle(open-nav-items")
       }
